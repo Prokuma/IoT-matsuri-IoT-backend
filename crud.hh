1403960866a8be8f2d701932c4776b485ab2edf7
Prokuma <prokuma@prokuma.dev>
2021-09-17 01:31:49 +0900

    [add] defined CRUD class and data models

---
+++ crud.hh
@@ -0,0 +1,33 @@
+#ifndef CRUD_HH
+#define CRUD_HH
+#include <map>
+#include <string>
+
+namespace models {
+    struct device {
+        std::string id;
+        std::string secret;
+        int user_id;
+        bool connection;
+    };
+
+    struct message {
+        std::string id;
+        std::string device_id;
+        std::string message;
+    };
+}
+
+
+class crud {
+    public:
+    models::device get_device(std::string device_id);
+    models::message get_message(std::string message_id);
+    std::map<std::string, std::string> get_messages_from_device_id(std::string device_id);
+    bool create_message(std::string device_id, std::string message);
+    std::string encrypt_message(std::string plain);
+    std::string decrypt_message(std::string crypted);
+};
+
+
+#endif
