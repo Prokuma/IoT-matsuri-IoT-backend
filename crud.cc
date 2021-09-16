d51c411eaa09ca54c17a10b44748baa1e50691fe
Prokuma <prokuma@prokuma.dev>
2021-09-17 03:13:16 +0900

    [add] implement encrypt and decrypt methods

--- crud.cc
+++ crud.cc
@@ -1,1 +1,37 @@
 #include "crud.hh"
+#include <cryptopp/filters.h>
+#include <string.h>
+#include <cryptopp/aes.h>
+#include <cryptopp/modes.h>
+#include <cryptopp/base64.h>
+
+std::string crud::encrypt_message(std::string plain, std::string key) {
+    std::string result;
+    CryptoPP::ECB_Mode<CryptoPP::AES>::Encryption ecb_encryptor(
+        (unsigned char*)key.c_str(),
+        CryptoPP::AES::MAX_KEYLENGTH
+    );
+
+    auto encryptor = new CryptoPP::StreamTransformationFilter(
+        ecb_encryptor,
+        new CryptoPP::StringSink(result)
+    );
+    CryptoPP::StringSource s(plain, true, encryptor);
+
+    return result;
+}
+
+std::string crud::decrypt_message(std::string crypted, std::string key) {
+    std::string result;
+    CryptoPP::ECB_Mode<CryptoPP::AES>::Decryption ecb_decryptor(
+        (unsigned char*)key.c_str(),
+        CryptoPP::AES::MAX_KEYLENGTH
+    );
+    auto decryptor = new CryptoPP::StreamTransformationFilter(
+        ecb_decryptor,
+        new CryptoPP::StringSink(result)
+    );
+    CryptoPP::StringSource s(crypted, true, decryptor);
+
+    return result;
+}
--- crud.hh
+++ crud.hh
@@ -3,6 +3,8 @@
 #include <map>
 #include <string>
 
+#define BUFSIZE 1024
+
 namespace models {
     struct device {
         std::string id;
@@ -25,8 +27,8 @@ class crud {
     models::message get_message(std::string message_id);
     std::map<std::string, std::string> get_messages_from_device_id(std::string device_id);
     bool create_message(std::string device_id, std::string message);
-    std::string encrypt_message(std::string plain);
-    std::string decrypt_message(std::string crypted);
+    std::string encrypt_message(std::string plain, std::string key);
+    std::string decrypt_message(std::string crypted, std::string key);
 };
 
 
